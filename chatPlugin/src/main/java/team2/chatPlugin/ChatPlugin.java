d3bf8ac7ffb5e31651f9584c1875c96a236789b1
TadianDang <tqdang1@uno.edu>
2025-04-28 01:43:04 -0500

    more Updates

--- chatPlugin/src/main/java/team2/chatPlugin/ChatPlugin.java
+++ chatPlugin/src/main/java/team2/chatPlugin/ChatPlugin.java
@@ -1,18 +1,20 @@
 package team2.chatPlugin;
 
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import team2.chatPlugin.commands.LLMCommand;
 
 public final class ChatPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-
+        getCommand("llm").setExecutor(new LLMCommand());
     }
 
     @Override
@@ -22,7 +24,11 @@ public final class ChatPlugin extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+        event.getPlayer().sendMessage(Component.text("§a[ChatPlugin]§f Hello, " + event.getPlayer().getName() + "!"));
     }
 
+    @EventHandler
+    public void onPlayerExit(PlayerQuitEvent event){
+        event.getPlayer().sendMessage(Component.text("§a[ChatPlugin]§f Goodbye, " + event.getPlayer().getName() + "!"));
+    }
 }
