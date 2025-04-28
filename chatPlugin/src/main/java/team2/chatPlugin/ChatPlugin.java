f550a19a8e1d5779da075bb66c22fbb9f917fbb5
TadianDang <tqdang1@uno.edu>
2025-04-28 00:19:04 -0500

    switch from gradle to maven

--- chatPlugin/src/main/java/team2/chatPlugin/ChatPlugin.java
+++ chatPlugin/src/main/java/team2/chatPlugin/ChatPlugin.java
@@ -1,20 +1,28 @@
 package team2.chatPlugin;
 
+import org.bukkit.plugin.java.JavaPlugin;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class ChatPlugin extends JavaPlugin implements Listener {
+public final class ChatPlugin extends JavaPlugin implements Listener {
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
     }
+
 }
