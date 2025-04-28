e227caf24d607af4dc87b30b777451ceef2d796a
Justin Nguyen <jhnguy16@uno.edu>
2025-04-28 02:20:01 -0500

    Delete commands.java

--- minecraft-project/commands.java
+++
@@ -1,8 +0,0 @@
-new CommandAPICommand("chatgpt")
-    .withArguments(new GreedyStringArgument("message"))
-    .withAliases("chat", "chatgpt", "gpt")
-    .executes((sender, args) -> {
-        String message = (String) args.get("message");
-        Bukkit.getServer().broadcastMessage(message);
-    })
-    .register();
